6f50d30e551c2970c083a53437e023a5007ba896
Mikkel Steenberg Kristoffersen <mikk6053@edu.eal.dk>
2017-12-13 17:25:59 +0100

    Booking & BookingRepo

---
+++ Projectstyrings/DomainBusiness/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projectstyrings
+{
+    public class Booking
+    {
+        public DateTime BookingTime { get; set; }
+        public string Equipment { get; set; }
+        public int EquipmentId { get; set; }
+
+        public Booking(DateTime dateTime, int equipmentId)
+        {
+            BookingTime = dateTime;
+            EquipmentId = equipmentId;
+        }
+    }
+}
